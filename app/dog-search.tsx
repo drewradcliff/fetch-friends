b17df6b1adfb246503733724281d7a353f17b9fd
Drew <sirampr@gmail.com>
2025-02-04 16:39:18 -0500

    add combobox

--- app/dog-search.tsx
+++ app/dog-search.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDown } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 import useSWR from "swr";
 
 type SearchResult = {
@@ -29,13 +44,16 @@ const fetcher = (url: string, options: RequestInit = {}) =>
   }).then((res) => res.json());
 
 export default function DogSearch() {
-  // const { data: breeds } = useSWR<string[]>(
-  //   process.env.NEXT_PUBLIC_FETCH_API_URL + "/dogs/breeds",
-  //   fetcher
-  // );
+  const [comboBoxValue, setComboBoxValue] = useState("");
+  const { data: breeds, isLoading: isBreedsLoading } = useSWR<string[]>(
+    process.env.NEXT_PUBLIC_FETCH_API_URL + "/dogs/breeds",
+    fetcher
+  );
 
   const { data: search } = useSWR<SearchResult>(
-    process.env.NEXT_PUBLIC_FETCH_API_URL + "/dogs/search",
+    process.env.NEXT_PUBLIC_FETCH_API_URL +
+      "/dogs/search" +
+      (comboBoxValue ? `?breeds=${[comboBoxValue]}` : ""),
     fetcher
   );
 
@@ -51,21 +69,28 @@ export default function DogSearch() {
       })
   );
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading || isBreedsLoading || !breeds) return <div>Loading...</div>;
 
   return (
-    <div>
-      <div className="grid grid-cols-5 gap-4">
+    <div className="max-w-screen-lg mx-auto">
+      <div className="flex flex-row gap-2 items-center py-4">
+        <ComboBox
+          breeds={breeds}
+          comboBoxValue={comboBoxValue}
+          setComboBoxValue={setComboBoxValue}
+        />
+      </div>
+      <div className="grid grid-cols-4 gap-8">
         {dogs?.map(({ id, img, name, age, breed }) => (
           <div
             key={id}
-            className="border border-gray-300 rounded-md overflow-hidden p-4 flex flex-col items-center justify-center h-[300px]"
+            className="border border-gray-300 rounded-xl overflow-hidden flex flex-col items-center justify-center h-[300px]"
           >
             <div className="relative w-[200px] h-[200px]">
               <Image src={img} alt={name} fill className="object-cover" />
             </div>
             <div className="flex flex-col gap-2">
-              <span>
+              <span className="flex flex-row gap-2 items-center">
                 <h3 className="text-lg font-bold">{name}</h3>
                 <p>{age}</p>
               </span>
@@ -75,5 +100,57 @@ export default function DogSearch() {
         ))}
       </div>
     </div>
+  );
+}
+
+function ComboBox({
+  breeds,
+  comboBoxValue,
+  setComboBoxValue,
+}: {
+  breeds: string[];
+  comboBoxValue: string;
+  setComboBoxValue: (value: string) => void;
+}) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          <span className="truncate">
+            {comboBoxValue
+              ? breeds.find((b) => b === comboBoxValue)
+              : "Select Breed"}
+          </span>
+          <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search Breeds" />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            {breeds.map((breed) => (
+              <CommandItem
+                key={breed}
+                value={breed}
+                onSelect={() => {
+                  setComboBoxValue(breed);
+                  setOpen(false);
+                }}
+              >
+                {breed}
+              </CommandItem>
+            ))}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 }
